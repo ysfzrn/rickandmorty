afef7eb4ef47cfdf0cd4511135572333814f17bf
Yusuf Zeren <ysfzrn@gmail.com>
2019-01-16 06:25:43 +0300

    ios splash

--- index.js
+++ index.js
@@ -4,12 +4,17 @@ import Routes from './src/routes';
 import {name as appName} from './app.json';
 import ApolloClient from 'apollo-boost';
 import {ApolloProvider} from 'react-apollo';
+import SplashScreen from 'react-native-splash-screen';
 
 const client = new ApolloClient({
   uri: 'https://rickandmortyapi.com/graphql'
 });
 
 class Root extends React.Component {
+  componentDidMount() {
+    SplashScreen.hide();
+  }
+
   render() {
     return (
       <ApolloProvider client={client}>
